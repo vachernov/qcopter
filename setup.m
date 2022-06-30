1d8491bb46d74578afffdf7b7fc88ceb5247aea9
vachernov <valery_che@icloud.com>
2022-06-30 15:35:25 +0300

    Quaternion-based orientation control tested

---
+++ setup.m
@@ -0,0 +1,49 @@
+%% Setup
+
+%% Plant params
+% general
+
+g = 9.81;
+
+% Inertias
+
+m = 1;
+
+I_xx = 0.04989;
+I_yy = 0.04989;
+I_zz = 0.24057;
+
+% Geometry
+
+b = 0.6;
+l = 0.6;
+d = 0.3;
+
+% Calulations
+
+I = [[I_xx,   0,     0]; 
+     [0,    I_yy,    0];
+     [0,      0,  I_zz]];
+ 
+Xi = [[(I_yy - I_xx)/I_zz,          0,                          0]; 
+      [0,                  (I_zz - I_xx)/I_yy,                  0];
+      [0,                           0,         (I_xx - I_yy)/I_zz]]; 
+  
+C_DIM = [[b,  0,  0,  0];
+         [0, b*l, 0,  0];
+         [0,  0, b*l, 0];
+         [0,  0,  0,  d]];
+     
+FB =    [[ 1,  1,  1, 1];    
+         [ 0, -1,  0, 1];
+         [-1,  0,  1, 0];
+         [-1,  1, -1, 1]];
+
+M2B = C_DIM*FB;    % [ Ω_1^2, Ω_2^2, Ω_3^2, Ω_4^2 ]^T ---> [ f_t, \tau_x, \tau_y, \tau_z ]^T
+
+M2B_INV = inv(M2B);
+ 
+%% Controller
+
+k_1 = -1;
+k_2 = -1;
